9d2ca2d518b7fe0e866be9bca81c3c5a1962451a
Gohlisch <tgohlisch@brockhaus-ag.de>
2022-08-10 11:31:59 +0200

    fix(frontend; IconButton): show icon if no text is given by adjusting its height

--- diversity-lunch-fe/src/components/Shared/Controlls/IconButton.tsx
+++ diversity-lunch-fe/src/components/Shared/Controlls/IconButton.tsx
@@ -1,5 +1,5 @@
-import {MouseEvent} from 'react';
-
+import { MouseEvent } from 'react';
+import '../../../styles/component-styles/shared/Controlls/IconButton.scss';
 
 export interface IconButtonProps {
     iconPath: string,
@@ -9,11 +9,10 @@ export interface IconButtonProps {
 }
 
 export const IconButton = ({iconPath, text, onClick, altText}: IconButtonProps) => {
-
-
+    const textElement = text ? <span>{text}</span> : null;
     return (
         <button onClick={onClick}>
-            <p>{text || ""}</p>
+            {textElement}
             <img src={iconPath} alt={altText}/>
         </button>
     )
