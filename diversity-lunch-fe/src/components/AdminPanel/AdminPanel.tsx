3c0e2b0c3b53a2ade2cfe3a1f4b1fe9683432000
Gohlisch <tgohlisch@brockhaus-ag.de>
2022-08-10 12:05:25 +0200

    feat(frontend; AdminPanel): Use IconButton

--- diversity-lunch-fe/src/components/AdminPanel/AdminPanel.tsx
+++ diversity-lunch-fe/src/components/AdminPanel/AdminPanel.tsx
@@ -1,18 +1,16 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { CloseSiteContainer } from '../General/HeaderTemplate/CloseSiteContainer';
 import { DiversityIconContainer } from '../General/HeaderTemplate/DiversityIconContainer';
 import { GenericList } from '../Shared/GenericList/GenericList';
 import { Project } from './Project';
-import {IconButton} from "../Shared/Controlls/IconButton";
+import { IconButton } from '../Shared/Controlls/IconButton';
 import iconMeeting from '../../resources/icons/icon-anstehende-termine.svg';
-
 
 interface AdminPanelListItemProp {
   item: Project
 }
 
-const AdminPanelListItem: FC<AdminPanelListItemProp>
-  = ({ item: project }: AdminPanelListItemProp) => <p>{project.descriptor}</p>;
+
 
 const data: Project[] = [{
   id: 0,
@@ -22,20 +20,53 @@ const data: Project[] = [{
   descriptor: 'world',
 }];
 
-export const AdminPanel: FC = () => (
-  <section>
-    <header className="Profile-logo-container">
-      <CloseSiteContainer />
-      <DiversityIconContainer title="ADMIN PANEL" />
-    </header>
-    <main>
+// TODO: die Referenz muss man (anscheinend) in React ändern, damit das Objekt in State zu einem
+// neuen Rendern führt. BITTE PRÜFEN OB DIES DIE BESTE METHODE IST, UM STATE MIT OBJECTS/ARRAYS
+// ZU VERWALTEN!
+function updateProjectDescriptor(projects: Project[], project: Project): Project[] {
+  for (const p of projects) {
+    if (p.id === project.id) {
+      p.descriptor = 'ja lol ey';
+      return projects;
+    }
+  }
+  return projects;
+}
+
+export const AdminPanel: FC = () => {
+  const [projects, setProjects] = useState(data);
+
+  const removeProject = (project: Project) => {
+    setProjects(projects.filter(p => p.id !== project.id));
+  }
+  const updateProject = (project: Project) => {
+    setProjects(updateProjectDescriptor(projects, project).slice());
+  }
+
+  const AdminPanelListItem: FC<AdminPanelListItemProp>
+    = ({ item: project }: AdminPanelListItemProp) => (
+    <article>
+      <span>{project.descriptor}</span>
+      <IconButton iconPath={iconMeeting} altText="Projekt bearbeiten" onClick={()=>updateProject(project)} ></IconButton>
+      <IconButton iconPath={iconMeeting} altText="Projekt löschen" onClick={()=>removeProject(project)}></IconButton>
+    </article>
+  );
+
+  return (
+    <section>
+      <header className="Profile-logo-container">
+        <CloseSiteContainer/>
+        <DiversityIconContainer title="ADMIN PANEL"/>
+      </header>
+      <main>
 
         <IconButton iconPath={iconMeeting} onClick={() => console.log('halo')} altText="Alt"/>
-      <GenericList
-        data={data}
-        getKeyFunction={(item) => item.id}
-        itemComponent={AdminPanelListItem}
-      />
-    </main>
-  </section>
-);
+        <GenericList
+          data={projects}
+          getKeyFunction={(item) => item.id}
+          itemComponent={AdminPanelListItem}
+        />
+      </main>
+    </section>
+  );
+};
