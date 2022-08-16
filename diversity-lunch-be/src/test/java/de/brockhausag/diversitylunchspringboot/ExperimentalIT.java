9225c86d8395ef6194e5004123042e21007fa72f
Lionel Pereira Azevedo <Lionel.PereiraAzevedo@brockhaus-ag.de>
2022-08-16 15:09:07 +0200

    build(backend; integration-tests): made experimental integration test succeed

--- diversity-lunch-be/src/test/java/de/brockhausag/diversitylunchspringboot/ExperimentalIT.java
+++ diversity-lunch-be/src/test/java/de/brockhausag/diversitylunchspringboot/ExperimentalIT.java
@@ -1,49 +1,34 @@
 package de.brockhausag.diversitylunchspringboot;
 
-import de.brockhausag.diversitylunchspringboot.data.ProfileTestdataFactory;
 import de.brockhausag.diversitylunchspringboot.email.service.DiversityLunchEMailService;
 import de.brockhausag.diversitylunchspringboot.properties.DiversityLunchMailProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.mail.javamail.JavaMailSender;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.Mockito.when;
 
-//@ExtendWith(SpringExtension.class)
 @SpringBootTest
-@Import(DiversityLunchMailProperties.class)
-//@ContextConfiguration(classes = {DiversityLunchMailProperties.class})
 @RequiredArgsConstructor
 public class ExperimentalIT {
 
-
-    private final ProfileTestdataFactory profileTestdataFactory = new ProfileTestdataFactory();
-
-    @Autowired
     private DiversityLunchEMailService diversityLunchEMailService;
 
-    //    @BeforeEach
-//    public void setUp() {
-//        this.diversityLunchEMailService = new DiversityLunchEMailService(
-//                this.javaMailSender,
-//                this.diversityLunchMailProperties);
-//    }
-
     @Autowired
-    private WebApplicationContext webApplicationContext;
+    private JavaMailSender mailSender;
 
-    private MockMvc mockMvc;
+    @Mock
+    private DiversityLunchMailProperties diversityLunchMailProperties;
+
 
     @BeforeEach
-    public void setup() throws Exception {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    void setUp() {
+        this.diversityLunchEMailService = new DiversityLunchEMailService(this.mailSender, this.diversityLunchMailProperties);
     }
 
     @SneakyThrows
@@ -54,11 +39,8 @@ public class ExperimentalIT {
         String bodyHtml = "subject";
         String bodyPlain = "subject";
 
-        this.diversityLunchEMailService.sendEmail(emailAddress, subject, bodyHtml, bodyPlain);
-//        this.mockMvc.perform(
-//                get("/mailing/sendTestMail")
-//                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + profileTestdataFactory.getTokenStringFromId("Test"))
-//        ).andExpect(status().isUnauthorized());
-    }
+        when(this.diversityLunchMailProperties.getSender()).thenReturn("test@test.de");
 
+        this.diversityLunchEMailService.sendEmail(emailAddress, subject, bodyHtml, bodyPlain);
+    }
 }
