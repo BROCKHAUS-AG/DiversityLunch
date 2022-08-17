f64bd7d2ab584c8747f3eede013f123131f7df61
janis5000 <jaarsel1@gmail.com>
2022-08-17 13:41:37 +0200

    feat(IntegrationTestEmail): Implemented test, which checks the response from mailhog after sending a mail.

--- diversity-lunch-be/src/test/java/de/brockhausag/diversitylunchspringboot/integrationstests/EMailControllerIT.java
+++ diversity-lunch-be/src/test/java/de/brockhausag/diversitylunchspringboot/integrationstests/EMailControllerIT.java
@@ -3,12 +3,14 @@ package de.brockhausag.diversitylunchspringboot.integrationstests;
 import de.brockhausag.diversitylunchspringboot.properties.DiversityLunchMailProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.apache.hc.client5.http.classic.methods.HttpDelete;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.classic.methods.HttpUriRequest;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -34,16 +38,19 @@ public class EMailControllerIT {
     @Autowired
     private WebApplicationContext appContext;
 
+    @SneakyThrows
     @BeforeEach
-    void setup() {
+    void setup(){
         mockMvc = MockMvcBuilders.webAppContextSetup(appContext).build();
-        // todo lazevedo 17.08.22 call mailhog v1 api to delete all prior messages
+
+        HttpUriRequest request = new HttpDelete( "http://localhost:8025/api/v1/messages");
+        CloseableHttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );
     }
 
     @SneakyThrows
     @Test
     public void testSendTestMail_expectedToNotThrowException() {
-        when(this.diversityLunchMailProperties.getSender()).thenReturn("test@test.de");
+        when(this.diversityLunchMailProperties.getSender()).thenReturn("diversitylunchtest@brockhaus-ag.de");
 
         mockMvc.perform(
                 post("/mailing/sendTestMail")
@@ -53,18 +60,39 @@ public class EMailControllerIT {
     @SneakyThrows
     @Test
     public void testSendTestMail_expectedToNotThrowException2() {
-        // todo lazevedo 17.08.22 rename and finish test
-        // 1. make sure all mails are deleted
-        // 2. send mail
-        // 3. check if received by mailhog correctly (sender, absender, subject, body, nur eine mail)
+        //Send email
+        when(this.diversityLunchMailProperties.getSender()).thenReturn("diversitylunchtest@brockhaus-ag.de");
 
-        // Given
-        HttpUriRequest request = new HttpGet( "http://localhost:8025/api/v1/messages");
+        mockMvc.perform(
+                post("/mailing/sendTestMail")
+        );
 
-        // When
+        // Prepare request to Mailhog
+        HttpUriRequest request = new HttpGet( "http://localhost:8025/api/v2/messages");
+
+        //Send request to Mailhog
         CloseableHttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );
 
+        //Get values from response
         String responseBody = EntityUtils.toString(httpResponse.getEntity());
         JSONObject jsonResponse = new JSONObject(responseBody);
+        String to = jsonResponse.getJSONArray("items").getJSONObject(0).getJSONObject("Content")
+                .getJSONObject("Headers").getJSONArray("To").getString(0);
+        String from = jsonResponse.getJSONArray("items").getJSONObject(0).getJSONObject("Content")
+                .getJSONObject("Headers").getJSONArray("From").getString(0);
+        String subject = jsonResponse.getJSONArray("items").getJSONObject(0).getJSONObject("Content")
+                .getJSONObject("Headers").getJSONArray("Subject").getString(0);
+        String body = jsonResponse.getJSONArray("items").getJSONObject(0).getJSONObject("MIME")
+                .getJSONArray("Parts").getJSONObject(0).getJSONObject("MIME")
+                .getJSONArray("Parts").getJSONObject(0).getJSONObject("MIME")
+                .getJSONArray("Parts").getJSONObject(0).getString("Body");
+        int amount = jsonResponse.getInt("total");
+
+        //Assert
+        Assertions.assertEquals("test@test.de",to);
+        Assertions.assertEquals("diversitylunchtest@brockhaus-ag.de", from);
+        Assertions.assertEquals("Testsubject",subject);
+        Assertions.assertEquals("Hallo :)", body);
+        Assertions.assertEquals(1, amount);
     }
 }
