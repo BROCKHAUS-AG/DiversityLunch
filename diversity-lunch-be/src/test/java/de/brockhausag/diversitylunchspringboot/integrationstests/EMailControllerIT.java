c01ecffc0963e5fa132b9e93f515903cad6be7a6
Lionel Pereira Azevedo <Lionel.PereiraAzevedo@brockhaus-ag.de>
2022-08-17 12:52:31 +0200

    build(backend; integration-tests): add preliminary test structure for mailhog related integration test

--- diversity-lunch-be/src/test/java/de/brockhausag/diversitylunchspringboot/integrationstests/EMailControllerIT.java
+++ diversity-lunch-be/src/test/java/de/brockhausag/diversitylunchspringboot/integrationstests/EMailControllerIT.java
@@ -3,10 +3,15 @@ package de.brockhausag.diversitylunchspringboot.integrationstests;
 import de.brockhausag.diversitylunchspringboot.properties.DiversityLunchMailProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpUriRequest;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -32,6 +37,7 @@ public class EMailControllerIT {
     @BeforeEach
     void setup() {
         mockMvc = MockMvcBuilders.webAppContextSetup(appContext).build();
+        // todo lazevedo 17.08.22 call mailhog v1 api to delete all prior messages
     }
 
     @SneakyThrows
@@ -42,5 +48,23 @@ public class EMailControllerIT {
         mockMvc.perform(
                 post("/mailing/sendTestMail")
         ).andExpect(status().isOk());
+    }
+
+    @SneakyThrows
+    @Test
+    public void testSendTestMail_expectedToNotThrowException2() {
+        // todo lazevedo 17.08.22 rename and finish test
+        // 1. make sure all mails are deleted
+        // 2. send mail
+        // 3. check if received by mailhog correctly (sender, absender, subject, body, nur eine mail)
+
+        // Given
+        HttpUriRequest request = new HttpGet( "http://localhost:8025/api/v1/messages");
+
+        // When
+        CloseableHttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );
+
+        String responseBody = EntityUtils.toString(httpResponse.getEntity());
+        JSONObject jsonResponse = new JSONObject(responseBody);
     }
 }
