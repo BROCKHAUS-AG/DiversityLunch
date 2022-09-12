44425c929e4cf0722fa8693e9bc14d807b80f9cb
Sebastian Harwig <sharwig@brockhaus-ag.de>
2022-09-12 13:37:26 +0200

    feat(profile.utils;): added methods for mapping of lists to mapper interface

--- diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/profile/utils/Mapper.java
+++ diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/profile/utils/Mapper.java
@@ -1,8 +1,14 @@
 package de.brockhausag.diversitylunchspringboot.profile.utils;
 
+import java.util.List;
+
 public interface Mapper<DtoType, EntityType> {
 
     DtoType entityToDto(EntityType entity);
 
     EntityType dtoToEntity(DtoType dto);
+
+    List<DtoType> entityToDto(List<EntityType> entity);
+
+    List<EntityType> dtoToEntity(List<DtoType> dto);
 }
