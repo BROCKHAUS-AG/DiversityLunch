b4aca1e3bf8f61b136966831464796e6201aab8e
Sebastian Harwig <sharwig@brockhaus-ag.de>
2022-09-13 11:00:31 +0200

    test(profile; ): implemented uni tests for CountryController

--- diversity-lunch-be/src/test/java/de/brockhausag/diversitylunchspringboot/profile/controller/CountryControllerTest.java
+++ diversity-lunch-be/src/test/java/de/brockhausag/diversitylunchspringboot/profile/controller/CountryControllerTest.java
@@ -13,7 +13,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import static org.junit.jupiter.api.Assertions.*;
-import java.util.Optional;
+
+import java.util.*;
 
 import static org.mockito.Mockito.when;
 
@@ -41,10 +42,72 @@ public class CountryControllerTest {
         when(countryMapper.entityToDto(existingCountryEntity)).thenReturn(expectedCountryDto);
 
         //Act
-        ResponseEntity<CountryDto> actualResponse = countryController.getCountry(existingCountryEntity.getId());
+        ResponseEntity<?> actualResponse = countryController.getCountry(existingCountryEntity.getId());
 
         //Assert
         assertEquals(expectedResponse.getStatusCode(),actualResponse.getStatusCode());
         assertEquals(expectedResponse.getBody(), actualResponse.getBody());
+    }
+
+    @Test
+    void testGetCountry_withNotExistingId_returnsNotFound() {
+        //Arrange
+        Long notExistingId = 666L;
+
+        when(countryService.getEntityById(notExistingId)).thenReturn(Optional.empty());
+
+        //Act
+        ResponseEntity<?> actualResponse = countryController.getCountry(notExistingId);
+
+        //Assert
+        assertFalse(actualResponse.hasBody());
+        assertEquals(HttpStatus.NOT_FOUND, actualResponse.getStatusCode());
+    }
+
+    @Test
+    void testGetAllCountries_withEmptyDb_returnsEmptyList(){
+        //Arrange
+        List<CountryDto> emptyDtoList = Collections.emptyList();
+        List<CountryEntity> emptyEntityList = Collections.emptyList();
+        ResponseEntity<List<CountryDto>> expectedResponse = new ResponseEntity<>(
+                emptyDtoList,
+                HttpStatus.OK
+        );
+
+        when(countryService.getAllEntities()).thenReturn(emptyEntityList);
+        when(countryMapper.entityToDto(emptyEntityList)).thenReturn(emptyDtoList);
+        //Act
+        ResponseEntity<List<CountryDto>> actualResponse = countryController.getAllCountries();
+
+        //Assert
+        assertEquals(expectedResponse.getBody(), actualResponse.getBody());
+        assertEquals(expectedResponse.getStatusCode(), actualResponse.getStatusCode());
+    }
+
+    @Test
+    void testGetAllCountries_withTwoDbEntries_returnsListOfTwoCountryEntities(){
+        //Arrange
+        CountryEntity firstCountryEntity = this.factory.buildCountryEntity();
+        CountryEntity secondCountryEntity = this.factory.buildSecondCountryEntity();
+        List<CountryEntity> countryEntityList = Arrays.asList(firstCountryEntity, secondCountryEntity);
+
+        CountryDto firstCountryDto = this.factory.buildCountryDto();
+        CountryDto secondCountryDto = this.factory.buildSecondCountryDto();
+        List<CountryDto> countryDtoList = Arrays.asList(firstCountryDto, secondCountryDto);
+
+        ResponseEntity<List<CountryDto>> expectedResponse = new ResponseEntity<>(
+                countryDtoList,
+                HttpStatus.OK
+        );
+
+        when(countryService.getAllEntities()).thenReturn(countryEntityList);
+        when(countryMapper.entityToDto(countryEntityList)).thenReturn(countryDtoList);
+        //Arrange
+        ResponseEntity<List<CountryDto>> actualResponse = countryController.getAllCountries();
+
+        //Assert
+        assertEquals(expectedResponse.getBody(), actualResponse.getBody());
+        assertEquals(expectedResponse.getStatusCode(), actualResponse.getStatusCode());
+
     }
 }
