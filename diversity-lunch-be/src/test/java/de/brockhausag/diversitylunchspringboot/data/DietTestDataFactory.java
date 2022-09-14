4c7e8c5e98fd25269407c70303d8481edbc44495
Sebastian Harwig <sharwig@brockhaus-ag.de>
2022-09-14 10:24:57 +0200

    test(BE; test; profile): added unit test and factory for diet controller and mapper

---
+++ diversity-lunch-be/src/test/java/de/brockhausag/diversitylunchspringboot/data/DietTestDataFactory.java
@@ -0,0 +1,44 @@
+package de.brockhausag.diversitylunchspringboot.data;
+
+import de.brockhausag.diversitylunchspringboot.profile.model.dtos.DietDto;
+import de.brockhausag.diversitylunchspringboot.profile.model.entities.DietEntity;
+
+public class DietTestDataFactory {
+
+    private final Long id = 7L;
+    private final String descriptor = "vegan";
+    private final Long secondId = 9L;
+    private final String secondDescriptor = "starve";
+
+    public DietEntity.DietEntityBuilder entityBuilder() {
+        return DietEntity.builder()
+                .id(id)
+                .descriptor(descriptor);
+    }
+    public DietEntity buildDietEntity() {
+        return entityBuilder().build();
+    }
+
+    public DietDto.DietDtoBuilder dtoBuilder() {
+        return DietDto.builder()
+                .id(id)
+                .descriptor(descriptor);
+    }
+    public DietDto buildDietDto() {
+        return dtoBuilder().build();
+    }
+
+    public DietEntity.DietEntityBuilder secondEntityBuilder(){
+        return DietEntity.builder()
+                .id(secondId)
+                .descriptor(secondDescriptor);
+    }
+    public DietEntity buildSecondDietEntity(){return secondEntityBuilder().build();}
+
+    public DietDto.DietDtoBuilder secondDtoBuilder(){
+        return DietDto.builder()
+                .id(secondId)
+                .descriptor(secondDescriptor);
+    }
+    public DietDto buildSecondDietDto(){return secondDtoBuilder().build();}
+}
