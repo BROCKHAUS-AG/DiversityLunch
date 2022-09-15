77505bb35c9f4c7354cffd4286a7463082abdbbb
tgohlisch <timgohlisch@gmail.com>
2022-09-15 12:05:59 +0200

    feat(backend; Security): set permissions for roles

--- diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/security/AccountRole.java
+++ diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/security/AccountRole.java
@@ -3,14 +3,24 @@ package de.brockhausag.diversitylunchspringboot.security;
 import com.google.common.collect.Sets;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Getter
 public enum AccountRole {
-    STANDARD(Sets.newHashSet()),
-    ADMIN(Sets.newHashSet());
+    STANDARD(Sets.newHashSet(AccountPermission.PROFILE_OPTION_READ)),
+    ADMIN(Sets.newHashSet(AccountPermission.PROFILE_OPTION_READ, AccountPermission.PROFILE_OPTION_WRITE));
 
     private final Set<AccountPermission> permissions;
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        final Set<SimpleGrantedAuthority> permissions = this.getPermissions()
+                .stream()
+                .map(p -> new SimpleGrantedAuthority(p.getName())).collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return permissions;
+    }
 }
