0111c567fa4c45d973681446716b4659c34778cd
janis5000 <jaarsel1@gmail.com>
2022-09-16 14:59:17 +0200

    feat(testing): WIP writing test for GenericFetch

--- diversity-lunch-fe/src/data/generic/__test__/GenericFetch.test.tsx
+++ diversity-lunch-fe/src/data/generic/__test__/GenericFetch.test.tsx
@@ -1,34 +1,17 @@
 import { Provider, useDispatch, useSelector } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
 import React, { useEffect } from 'react';
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { render } from '@testing-library/react';
-import { screen } from '@testing-library/dom';
-import { authenticatedFetchGet } from '../../../utils/fetch.utils';
+import { render, screen } from '@testing-library/react';
 import * as fetcher from '../../../utils/fetch.utils';
-import * as generics from '../GenericFetch';
-import { GenericSlice } from '../GenericSlice';
+import { authenticatedFetchGet } from '../../../utils/fetch.utils';
 import { Country } from '../../../model/Country';
-import Yee from './Yee';
 import countryReducer from '../../country/country-reducer';
 import { countryFetch } from '../../country/fetch-country';
-import { meetingsReducer } from '../../meeting/meetings.reducer';
-import { authenticationReducer } from '../../authentication/authentication.reducer';
-import { profileReducer } from '../../profile/profile.reducer';
-import { accountReducer } from '../../account/account.reducer';
-import experienceLevelReducer from '../../experienceLevel/experience-level.reducer';
-import dietReducer from '../../diet/diet-reducer';
-import activityReducer from '../../activity/activity-reducer';
-import categoryReducer from '../../category/category-reducer';
-import cultureReducer from '../../culture/culture-reducer';
-import educationReducer from '../../education/education-reducer';
-import genderReducer from '../../gender/gender-reducer';
-import hobbyReducer from '../../hobby/hobby-reducer';
-import industryReducer from '../../industry/industry-reducer';
+import { APP_STORE, AppStoreState } from '../../../store/Store';
 
-/*const data = [{
+const data = [{
     id: 1,
     descriptor: 'Deutschland',
 }];
@@ -49,18 +32,19 @@ const getCountryResponse = async () => new Response(JSON.stringify(data));
 
 const getAllResponse = async () => new Response(JSON.stringify(data));
 
-export const TestComponent = () => {
+export const TestComponent = (props : any) => {
     jest.spyOn(fetcher, 'authenticatedFetchGet').mockReturnValue(getCountryResponse());
 
-    const countries = useSelector((store: storeState) => store.test);
+    const countries = useSelector((store: AppStoreState) => store.country);
     const dispatch = useDispatch();
 
-    dispatch(countryFetch.getAll());
+    dispatch(countryFetch.getAll()).then(props.callback);
 
     return (
-        <div data-testid="testja">
+        <section data-testid="testja">
+            <p>HI</p>
             {countries.items.map((elem : Country) => <p>{elem.descriptor}</p>)}
-        </div>
+        </section>
     );
 };
 
@@ -75,25 +59,18 @@ it('should fetch the data', async () => {
 });
 
 it('a', async () => {
-
-
-    render(
-        <Provider store={appStore}>
-            <div>
-                <TestComponent />
-            </div>
-        </Provider>,
-    );
+    let container : HTMLElement;
+    let expectCallback = () => expect(container.querySelectorAll('section')[0].textContent).toEqual('Deutschland');
+    ({ container } = render(<Provider store={APP_STORE}><TestComponent callback={expectCallback} /></Provider>));
     // const appStore = createStore(countryReducer, { items: [], fetched: false });
-
 
     // jest.spyOn(countryFetch, 'getAll').mockReturnValue(getAllResponse());
 
     // appStore.dispatch(() => countryFetch.getAll());
 
     // expect(appStore.getState().test.items).toEqual(null);
-
-    expect(screen.getByTestId('testja').children.length).toBe(3);
+    //await expect(container.querySelectorAll('section')[0].textContent).toEqual('Deutschland');
+    //expect(container.querySelectorAll('div')[1].innerContent).toEqual('Deutschland');
 
     // return (
     //     <Provider store={appStore}>
@@ -120,5 +97,4 @@ it('a', async () => {
     //
     // jest.spyOn(global, 'getAll').mockReturnValue();
     // dispatch(countryFetch.getAll());
-});*/
-
+});
