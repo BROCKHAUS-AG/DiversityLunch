f27286ce87b43cdab0f5ca6873911d5b9b46db34
Sebastian Harwig <sharwig@brockhaus-ag.de>
2022-09-19 12:22:52 +0200

    feat(BE; test): updated profile controller test

--- diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/project/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package de.brockhausag.diversitylunchspringboot.project.repository;
-
-import de.brockhausag.diversitylunchspringboot.project.model.ProjectEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProjectRepository extends CrudRepository<ProjectEntity, Long> {
-}
