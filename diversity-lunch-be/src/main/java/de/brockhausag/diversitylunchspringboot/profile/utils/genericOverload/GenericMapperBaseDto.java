301c2b66793e92a6a9f51dfd3f78cd94ecad72af
Sebastian Harwig <sharwig@brockhaus-ag.de>
2022-09-21 08:44:23 +0200

    feat(BE; profile utils): added generic mapper for base models

--- diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/profile/utils/genericOverload/GenericMapperBaseDto.java
+++
@@ -1,9 +0,0 @@
-package de.brockhausag.diversitylunchspringboot.profile.utils.genericOverload;
-
-public interface GenericMapperBaseDto<T> {
-    Long getId();
-    void setId(Long id);
-    String getDescriptor();
-    void setDescriptor(String descriptor);
-    T clone();
-}
--- diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/profile/utils/genericOverload/GenericMapperBaseEntity.java
+++
@@ -1,9 +0,0 @@
-package de.brockhausag.diversitylunchspringboot.profile.utils.genericOverload;
-
-public interface GenericMapperBaseEntity<T> {
-    Long getId();
-    void setId(Long id);
-    String getDescriptor();
-    void setDescriptor(String descriptor);
-    T clone();
-}
