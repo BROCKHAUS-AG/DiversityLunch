40c52665ef6e33026185dc3e09cfe6666b91b4e9
Sebastian Harwig <sharwig@brockhaus-ag.de>
2022-09-22 11:35:01 +0200

    refactor(BE; utils): WIP: refactor base models, services and mappers

--- diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/profile/utils/baseApi/BaseEntity.java
+++ diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/profile/utils/baseApi/BaseEntity.java
@@ -1,8 +1,25 @@
 package de.brockhausag.diversitylunchspringboot.profile.utils.baseApi;
 
-public interface BaseEntity {
-    Long getId();
-    void setId(Long id);
-    String getDescriptor();
-    void setDescriptor(String descriptor);
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String descriptor;
+
+    public int hashCode() {
+        String hashString = this.id.toString() + this.descriptor;
+        return hashString.hashCode() ;
+    }
+
 }
