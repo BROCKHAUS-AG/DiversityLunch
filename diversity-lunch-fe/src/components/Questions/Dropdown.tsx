37e98a41ed231113b4be3a2c4a97d247e0b3120a
tgohlisch <timgohlisch@gmail.com>
2022-09-26 15:45:05 +0200

    refactor(frontend): rename some props in Dropdown.tsx and profile-form.tsx

--- diversity-lunch-fe/src/components/Questions/Dropdown.tsx
+++ diversity-lunch-fe/src/components/Questions/Dropdown.tsx
@@ -6,32 +6,30 @@ import '../../styles/component-styles/questions/dropDown.scss';
 
 interface dropdownProps<T extends Identifiable> {
     options: T[];
-    label: string;
-    onChange: (selected : T) => void;
     placeholder: string;
+    onChange: (selected : T) => void;
+    label: string;
     // eslint-disable-next-line react/require-default-props
     currentValue?: T;
 }
 
 export const Dropdown = <T extends Identifiable>({
-    options, label, onChange, placeholder, currentValue,
+    options, placeholder, onChange, label, currentValue,
 }: dropdownProps<T>) => {
     const callOnChange = (_ : ChangeEvent<{}>, selected : T | null) => {
         if (!selected) return;
         onChange(selected);
     };
 
-    // const currentViewValue = options.find((o) => o.id === currentValue?.id);
-
     return (
         <div className="DropdownQuestion">
-            <p className="DropdownQuestion-question">{label}</p>
+            <p className="DropdownQuestion-question">{placeholder}</p>
             <Autocomplete
                 options={options}
                 getOptionLabel={(o) => o.descriptor}
                 renderInput={
                     // eslint-disable-next-line react/jsx-props-no-spreading
-                    (params: AutocompleteRenderInputParams) => <TextField {...params} variant="outlined" label={placeholder} />
+                    (params: AutocompleteRenderInputParams) => <TextField {...params} variant="outlined" label={label} />
                 }
                 onChange={callOnChange}
                 defaultValue={currentValue}
