76e723abeeaeaee2a3be20ad2ceea5888342ed09
tgohlisch <timgohlisch@gmail.com>
2022-09-26 12:18:34 +0200

    test(frontend): move data for integration tests to __global_test_data___

---
+++ diversity-lunch-fe/src/__global_test_data__/data.test.ts
@@ -0,0 +1,11 @@
+export const categoryData = [{ id: 2, descriptor: 'Kreatives' }, { id: 1337, descriptor: 'Sport' }];
+export const countryData = [{ id: 9, descriptor: 'Bahamas' }, { id: 1337, descriptor: 'Deutschland' }];
+export const dietData = [{ id: 9, descriptor: 'Fleischesser' }, { id: 1337, descriptor: 'Frutarier' }];
+export const educationData = [{ id: 9, descriptor: 'Abitur' }, { id: 1337, descriptor: 'Doktor' }];
+export const genderData = [{ id: 9, descriptor: 'männlich' }, { id: 1337, descriptor: 'weiblich' }];
+export const languageData = [{ id: 9, descriptor: 'Deutsch' }, { id: 1337, descriptor: 'Französisch' }];
+export const projectData = [{ id: 9, descriptor: 'intern' }, { id: 1337, descriptor: 'extern' }];
+export const religionData = [{ id: 9, descriptor: 'katholisch' }, { id: 1337, descriptor: 'evangelisch' }];
+export const workExperienceData = [{ id: 9, descriptor: '0-4 Jahre' }, { id: 1337, descriptor: '4-10 Jahre' }];
+export const hobbyData = [{ id: 6, descriptor: 'DIY', category: { id: 2, descriptor: 'Kreatives' } },
+    { id: 7, descriptor: 'Fußball', category: { id: 1, descriptor: 'Sport' } }];
--- diversity-lunch-fe/src/components/Profile/__test__/ProfileOverview.test.tsx
+++ diversity-lunch-fe/src/components/Profile/__test__/ProfileOverview.test.tsx
@@ -1,20 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { FC, useEffect } from 'react';
 
-import { Provider } from 'react-redux';
+import { Provider, useDispatch } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { ProfileOverview } from '../ProfileOverview';
 import { APP_STORE } from '../../../store/Store';
 import * as fetcher from '../../../utils/fetch.utils';
-import { CATEGORY_ENDPOINT } from '../../../data/category/fetch-category';
-import { COUNTRY_ENDPOINT } from '../../../data/country/fetch-country';
-import { DIET_ENDPOINT } from '../../../data/diet/fetch-diet';
-import { EDUCATION_ENDPOINT } from '../../../data/education/fetch-education';
-import { GENDER_ENDPOINT } from '../../../data/gender/fetch-gender';
-import { LANGUAGE_ENDPOINT } from '../../../data/language/language-fetch';
-import { PROJECT_ENDPOINT } from '../../../data/project/project-fetch';
-import { RELIGION_ENDPOINT } from '../../../data/religion/religion-fetch';
-import { WORK_EXPERIENCE_ENDPOINT } from '../../../data/work-experience/work-experience-fetch';
-import { HOBBY_ENDPOINT } from '../../../data/hobby/fetch-hobby';
+import { mockedFetchGet } from '../../../__global_test_data__/fetch.test';
+import { loadProfile } from '../../../data/profile/profile.actions';
 
 const data = {
     id: 4,
@@ -34,59 +27,34 @@ const data = {
 
 describe('Profile Overview', () => {
     let preFilledProfileDataValues: string[];
-    let providerElement: JSX.Element;
+    let ProfileLoader: FC;
     let container: HTMLElement;
     let col: HTMLCollection | never[];
 
     beforeEach(() => {
-        jest.spyOn(fetcher, 'authenticatedFetchGet').mockImplementation(
-            async (url: string) => {
-                if (url.includes(CATEGORY_ENDPOINT)) {
-                    return new Response(JSON.stringify([{ id: 2, descriptor: 'Kreatives' }, { id: 1337, descriptor: 'Sport' }]));
-                }
-                if (url.includes(COUNTRY_ENDPOINT)) {
-                    return new Response(JSON.stringify([{ id: 9, descriptor: 'Bahamas' }, { id: 1337, descriptor: 'Deutschland' }]));
-                }
-                if (url.includes(DIET_ENDPOINT)) {
-                    return new Response(JSON.stringify([{ id: 9, descriptor: 'Fleischesser' }, { id: 1337, descriptor: 'Frutarier' }]));
-                }
-                if (url.includes(EDUCATION_ENDPOINT)) {
-                    return new Response(JSON.stringify([{ id: 9, descriptor: 'Abitur' }, { id: 1337, descriptor: 'Doktor' }]));
-                }
-                if (url.includes(GENDER_ENDPOINT)) {
-                    return new Response(JSON.stringify([{ id: 9, descriptor: 'männlich' }, { id: 1337, descriptor: 'weiblich' }]));
-                }
-                if (url.includes(LANGUAGE_ENDPOINT)) {
-                    return new Response(JSON.stringify([{ id: 9, descriptor: 'Deutsch' }, { id: 1337, descriptor: 'Französisch' }]));
-                }
-                if (url.includes(PROJECT_ENDPOINT)) {
-                    return new Response(JSON.stringify([{ id: 9, descriptor: 'intern' }, { id: 1337, descriptor: 'extern' }]));
-                }
-                if (url.includes(RELIGION_ENDPOINT)) {
-                    return new Response(JSON.stringify([{ id: 9, descriptor: 'katholisch' }, { id: 1337, descriptor: 'evangelisch' }]));
-                }
-                if (url.includes(WORK_EXPERIENCE_ENDPOINT)) {
-                    return new Response(JSON.stringify([{ id: 9, descriptor: '0-4 Jahre' }, { id: 1337, descriptor: '4-10 Jahre' }]));
-                }
-                if (url.includes(HOBBY_ENDPOINT)) {
-                    return new Response(JSON.stringify([{ id: 6, descriptor: 'DIY', category: { id: 2, descriptor: 'Kreatives' } },
-                        { id: 7, descriptor: 'Fußball', category: { id: 1, descriptor: 'Sport' } }]));
-                }
-                return new Response('');
-            },
-        );
+        jest.spyOn(fetcher, 'authenticatedFetchGet').mockImplementation(mockedFetchGet);
 
-        providerElement = (
-            <Provider store={APP_STORE}>
+        ProfileLoader = function () {
+            const dispatch = useDispatch();
+            useEffect(() => { dispatch(loadProfile(data.id)); }, []);
+
+            return (
                 <ProfileOverview />
-            </Provider>
-        );
+            );
+        };
 
-        ({ container } = render(<BrowserRouter>{ providerElement }</BrowserRouter>));
+        ({ container } = render(
+            <BrowserRouter>
+                <Provider store={APP_STORE}>
+                    <ProfileLoader />
+                </Provider>
+            </BrowserRouter>,
+        ));
         col = container.children.item(0)?.children.item(2)?.children || [];
     });
-    it('render component without crashing', () => {
-        expect(container.firstChild!.firstChild).toHaveClass('Profile-logo-container');
+    it('render component without crashing', async () => {
+        const result = await screen.findByRole('img');
+        expect(result).toBeInTheDocument();
     });
     it('profile edit form diversity logo space', () => {
         const logoElement = container.children.item(0)?.children.item(0);
