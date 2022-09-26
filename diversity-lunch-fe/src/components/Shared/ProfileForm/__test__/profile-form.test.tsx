a44b1caeae6d118e2d5fb17492c3e1752818f131
janis5000 <jaarsel1@gmail.com>
2022-09-26 13:58:02 +0200

    feat(frontend): Added test for save button in profile-form

--- diversity-lunch-fe/src/components/Shared/ProfileForm/__test__/profile-form.test.tsx
+++ diversity-lunch-fe/src/components/Shared/ProfileForm/__test__/profile-form.test.tsx
@@ -1,28 +1,39 @@
 import { render, screen } from '@testing-library/react';
-import { FC, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { APP_STORE } from '../../../../store/Store';
 import { ProfileForm } from '../profile-form';
-import { profileData } from '../../../../__global_test_data__/data.test';
-import {isUpdatedProfile, isValidProfile} from '../../../../utils/validators/profile-validator';
-import {Profile} from "../../../../model/Profile";
+import { profileData } from '../../../../__global_test_data__/data';
+import { isUpdatedProfile, isValidProfile } from '../../../../utils/validators/profile-validator';
+import { Profile } from '../../../../model/Profile';
+import * as fetcher from '../../../../utils/fetch.utils';
+import { mockedFetchGet } from '../../../../__global_test_data__/fetch';
 
 describe('Profile form', () => {
-    const isUpdated = (updatedProfile: Partial<Profile>) => isValidProfile(updatedProfile) && isUpdatedProfile(updatedProfile as Profile, profile);
-    const renderContainer = (component : typeof ProfileForm) =>
-        render (<BrowserRouter>
+    const renderContainer = (component : JSX.Element) => render(
+        <BrowserRouter>
             <Provider store={APP_STORE}>
                 {component}
             </Provider>
-        </BrowserRouter>)
-
+        </BrowserRouter>,
+    ).container;
 
     beforeEach(() => {
 
     });
 
-    it('', () => {
-        renderContainer(<ProfileForm initialProfile={profileData[0]} checkValidity={() => false} onSubmit={() => {}}>)
+    it('should disable the button if checkValidity returns false', async () => {
+        jest.spyOn(fetcher, 'authenticatedFetchGet')
+            .mockImplementation(mockedFetchGet);
+        renderContainer(<ProfileForm initialProfile={profileData[0]} checkValidity={() => false} onSubmit={() => {}} />);
+        const result = await screen.findByText('Speichern');
+        expect(result).toBeDisabled();
+    });
+    it('should enable the button if checkValidity returns true', async () => {
+        jest.spyOn(fetcher, 'authenticatedFetchGet')
+            .mockImplementation(mockedFetchGet);
+        renderContainer(<ProfileForm initialProfile={profileData[0]} checkValidity={() => true} onSubmit={() => {}} />);
+        const result = await screen.findByText('Speichern');
+        expect(result).toBeEnabled();
     });
 });
