a10e2a03e593763f8bbe5ccd4c24677a16b43932
joschuatonn <joschua@francoismechanics.com>
2022-09-26 13:41:42 +0200

    feat(frontend): Created profile form test (WIP)

--- diversity-lunch-fe/src/components/Shared/ProfileForm/__test__/profile-form.test.tsx
+++ diversity-lunch-fe/src/components/Shared/ProfileForm/__test__/profile-form.test.tsx
@@ -1,1 +1,28 @@
-export const f = undefined;
+import { render, screen } from '@testing-library/react';
+import { FC, useEffect } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { APP_STORE } from '../../../../store/Store';
+import { ProfileForm } from '../profile-form';
+import { profileData } from '../../../../__global_test_data__/data.test';
+import {isUpdatedProfile, isValidProfile} from '../../../../utils/validators/profile-validator';
+import {Profile} from "../../../../model/Profile";
+
+describe('Profile form', () => {
+    const isUpdated = (updatedProfile: Partial<Profile>) => isValidProfile(updatedProfile) && isUpdatedProfile(updatedProfile as Profile, profile);
+    const renderContainer = (component : typeof ProfileForm) =>
+        render (<BrowserRouter>
+            <Provider store={APP_STORE}>
+                {component}
+            </Provider>
+        </BrowserRouter>)
+
+
+    beforeEach(() => {
+
+    });
+
+    it('', () => {
+        renderContainer(<ProfileForm initialProfile={profileData[0]} checkValidity={() => false} onSubmit={() => {}}>)
+    });
+});
