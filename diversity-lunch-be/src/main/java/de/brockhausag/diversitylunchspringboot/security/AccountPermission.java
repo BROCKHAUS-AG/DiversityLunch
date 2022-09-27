7a8dbb5ce31b6b319f1c5406b9074fa6f3919fb6
tgohlisch <timgohlisch@gmail.com>
2022-09-27 11:14:16 +0200

    feat(AccountRole, AccountPermission): Give ADMIN permission to assign the ADMIN role and AZURE_ADMIN permission to assign or withdraw the ADMIN

--- diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/security/AccountPermission.java
+++ diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/security/AccountPermission.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum AccountPermission {
     PROFILE_OPTION_READ("profile_option:read"),
-    PROFILE_OPTION_WRITE("profile_option:write");
+    PROFILE_OPTION_WRITE("profile_option:write"),
+    ADMIN_ROLE_ASSIGN("admin_role:assign"),
+    ADMIN_ROLE_WITHDRAW("admin_role:with_draw");
 
     private final String name;
 }
