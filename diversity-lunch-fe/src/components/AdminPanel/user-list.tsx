650019fb32708eec7ebebb5e2dd20c2a6015dab4
Fabian Senftleben <Fabian.Senftleben@brockhaus-ag.de>
2022-10-14 12:48:44 +0200

    feat(user-list): WIP collapse button for userlist

--- diversity-lunch-fe/src/components/AdminPanel/user-list.tsx
+++ diversity-lunch-fe/src/components/AdminPanel/user-list.tsx
@@ -3,7 +3,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import azureAdminLogo from '../../resources/icons/azure_modern.svg';
 import { AppStoreState } from '../../store/Store';
 import { AccountsState } from '../../data/accounts/accounts-reducer';
-import { assignAdminRole, getAllAccounts, revokeAdminRole } from '../../data/accounts/accounts-fetch';
+import {
+    assignAdminRole,
+    getAllAccounts,
+    revokeAdminRole,
+} from '../../data/accounts/accounts-fetch';
 import { ProfilesState } from '../../data/profiles/profiles-reducer';
 import { getAllProfiles } from '../../data/profiles/profiles-fetch';
 import { User } from './userAdministration/User';
@@ -26,12 +30,15 @@ export const UserList: FC = () => {
     }
     const mapAccountandProfileToUser = () => {
         const userList: User[] = [];
-        const accountList : Account[] = accountsState.items;
-        const profileList : Profile[] = profilesState.items;
+        const accountList: Account[] = accountsState.items;
+        const profileList: Profile[] = profilesState.items;
 
         profileList.forEach((profile) => {
-            const account : Account = accountList.filter((v) => v.profileId === profile.id)[0];
-            userList.push({ profile, account });
+            const account: Account = accountList.filter((v) => v.profileId === profile.id)[0];
+            userList.push({
+                profile,
+                account,
+            });
         });
 
         return userList;
@@ -44,10 +51,11 @@ export const UserList: FC = () => {
         dispatch(revokeAdminRole(accountId));
     };
 
-    const generateAdminListButton = (user : User) => {
+    const generateAdminListButton = (user: User) => {
         if (user.account.role === Role.ADMIN) {
             return <button onClick={() => revokeAdmin(user.account.profileId)}>-</button>;
-        } if (user.account.role === Role.STANDARD) {
+        }
+        if (user.account.role === Role.STANDARD) {
             return <button onClick={() => assignAdmin(user.account.profileId)}>+</button>;
         }
         return (
@@ -55,25 +63,32 @@ export const UserList: FC = () => {
         );
     };
 
-    return (
+    const collapseHtml = () => (
         <div className="optionsListContainer">
             <p className="editListTitle">Users</p>
             <div>
                 <section>
-                    {mapAccountandProfileToUser().map((user) => (
+                    {mapAccountandProfileToUser()
+                        .map((user) => (
 
-                        <section className="usersList" key={user.account.profileId}>
-                            <span>
-                                {user.profile.email}
-                            </span>
-                            <span>
-                                {user.account.role}
-                            </span>
-                            {generateAdminListButton(user)}
-                        </section>
-                    ))}
+                            <section className="usersList" key={user.account.profileId}>
+                                <span>
+                                    {user.profile.email}
+                                </span>
+                                <span>
+                                    {user.account.role}
+                                </span>
+                                {generateAdminListButton(user)}
+                            </section>
+                        ))}
                 </section>
             </div>
+        </div>
+    );
+
+    return (
+        <div>
+            {collapseHtml()}
         </div>
     );
 };
