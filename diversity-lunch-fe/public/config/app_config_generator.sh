f678a67233c8b83c1c6a7cc65ce4bdd5a5aef387
Sebastian Harwig <sharwig@brockhaus-ag.de>
2022-10-19 20:13:53 +0200

    feat(FE; docker): WIP: build working fe image through Dockerfile

--- diversity-lunch-fe/public/config/app_config_generator.sh
+++ diversity-lunch-fe/public/config/app_config_generator.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-file_path=/usr/share/nginx/html/config/app-config.js
+FILE_PATH=/usr/share/nginx/html/config/app-config.js
 
 echo "window.appConfig = {
-  REACT_APP_OIDC_CLIENT_ID: test,
-  REACT_APP_OIDC_SCOPE: test ,
-  REACT_APP_OIDC_CONFIG_ENDPOINT: test,
-  REACT_APP_OIDC_REDIRECT_URI: test,
-  REACT_APP_OIDC_AUTHORIZATION_ENDPOINT: test,
-};" > $file_path
+  REACT_APP_OIDC_CLIENT_ID: 'OIDC-Client-Id',
+  REACT_APP_OIDC_SCOPE: 'OIDC-Scopes',
+  REACT_APP_OIDC_CONFIG_ENDPOINT: 'OIDC-Well-Know-Endpoint',
+  REACT_APP_OIDC_REDIRECT_URI: 'OIDC-Redirect-URI',
+  REACT_APP_OIDC_AUTHORIZATION_ENDPOINT: 'OIDC-Authorization-Endpoint',
+};" > $FILE_PATH
+
+sed -i -- "s%OIDC-Client-Id%$CLIENT_ID%g" $FILE_PATH \
+&& sed -i -- "s%OIDC-Scopes%$OIDC_SCOPE%g" $FILE_PATH \
+&& sed -i -- "s%OIDC-Well-Know-Endpoint%$OIDC_CONFIG_ENDPOINT%g"  $FILE_PATH \
+&& sed -i -- "s%OIDC-Redirect-URI%$OIDC_REDIRECT_URI%g" $FILE_PATH \
+&& sed -i -- "s%OIDC-Authorization-Endpoint%$OIDC_AUTHORIZATION_ENDPOINT%g" $FILE_PATH
