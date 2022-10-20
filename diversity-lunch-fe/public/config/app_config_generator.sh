cc5e562c69f464806bacba04703d47642add925c
dfuerst <dfuerst@brockhaus-ag.de>
2022-10-20 11:10:19 +0200

    feat(FE, Docker Env Variables): WIP Working script, Missing docker entrypoint

--- diversity-lunch-fe/public/config/app_config_generator.sh
+++ diversity-lunch-fe/public/config/app_config_generator.sh
@@ -2,7 +2,8 @@
 FILE_PATH="/usr/share/nginx/html/config/app-config.js"
 rm -f $FILE_PATH
 touch $FILE_PATH
-echo "REACT_APP_OIDC_CLIENT_ID: 'OIDC-Client-Id',
+echo "window.appConfig = {
+  REACT_APP_OIDC_CLIENT_ID: 'OIDC-Client-Id',
   REACT_APP_OIDC_SCOPE: 'OIDC-Scopes',
   REACT_APP_OIDC_CONFIG_ENDPOINT: 'OIDC-Well-Know-Endpoint',
   REACT_APP_OIDC_REDIRECT_URI: 'OIDC-Redirect-URI',
