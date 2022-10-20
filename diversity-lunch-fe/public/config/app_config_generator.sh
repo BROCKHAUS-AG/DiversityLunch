72b5245379780ff3eef9278cb8e415aec4f1a685
XammaXJammaJ <jamelabdelhafidh@hotmail.com>
2022-10-20 12:03:22 +0200

    refactor(FE, Dockerfile): Script for env. is now started

--- diversity-lunch-fe/public/config/app_config_generator.sh
+++ diversity-lunch-fe/public/config/app_config_generator.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-FILE_PATH="/usr/share/nginx/html/config/app-config.js"
+FILE_PATH="./app-config.js"
 rm -f $FILE_PATH
 touch $FILE_PATH
+echo "STARTING"
 echo "window.appConfig = {
   REACT_APP_OIDC_CLIENT_ID: 'OIDC-Client-Id',
   REACT_APP_OIDC_SCOPE: 'OIDC-Scopes',
@@ -14,3 +15,4 @@ sed -i -- "s%OIDC-Scopes%$REACT_APP_OIDC_SCOPE%g" $FILE_PATH
 sed -i -- "s%OIDC-Well-Know-Endpoint%$REACT_APP_OIDC_CONFIG_ENDPOINT%g"  $FILE_PATH
 sed -i -- "s%OIDC-Redirect-URI%$REACT_APP_OIDC_REDIRECT_URI%g" $FILE_PATH
 sed -i -- "s%OIDC-Authorization-Endpoint%$REACT_APP_OIDC_AUTHORIZATION_ENDPOINT%g" $FILE_PATH
+echo "FINISHED"
