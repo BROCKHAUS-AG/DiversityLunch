eed2265a791973051d4e63c103f18d7d97fdf3c6
Lucas Karle <lcskrl@googlemail.com>
2022-10-25 12:15:36 +0200

    feat(BE Voucher): created Models

---
+++ diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/voucher/model/VoucherEntity.java
@@ -0,0 +1,28 @@
+package de.brockhausag.diversitylunchspringboot.voucher.model;
+
+import de.brockhausag.diversitylunchspringboot.meeting.model.MeetingEntity;
+import de.brockhausag.diversitylunchspringboot.profile.model.entities.ProfileEntity;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.util.UUID;
+
+@Entity
+@RequiredArgsConstructor
+@Builder
+public class VoucherEntity {
+    @GeneratedValue
+    @Id
+    private UUID uuid;
+
+    @OneToOne
+    private ProfileEntity profile;
+
+    private String voucherCode;
+
+    @OneToOne
+    private MeetingEntity meeting;
+}
