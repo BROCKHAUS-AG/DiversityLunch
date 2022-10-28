63926cb0d6fddb8e3a3024e4546238b895181e82
Fabian Senftleben <Fabian.Senftleben@brockhaus-ag.de>
2022-10-28 08:41:54 +0200

    fix(VoucherSCVHelper): Fix bug that the InputStream gets saved instead of the vouchers

--- diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/voucher/utils/VoucherCSVHelper.java
+++ diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/voucher/utils/VoucherCSVHelper.java
@@ -1,33 +1,22 @@
 package de.brockhausag.diversitylunchspringboot.voucher.utils;
 
-import de.brockhausag.diversitylunchspringboot.voucher.model.VoucherDto;
 import de.brockhausag.diversitylunchspringboot.voucher.model.VoucherEntity;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 public class VoucherCSVHelper {
-    public static String TYPE = "text/csv";
-    static String[] HEADERs = {"voucherCode"};
-
-    public static boolean hasCSVFormat(MultipartFile file) {
-
-        if (!TYPE.equals(file.getContentType())) {
-            return false;
-        }
-
-        return true;
-    }
 
     public static List<VoucherEntity> csvToVoucherEntities(InputStream is) {
         try {
             List<VoucherEntity> voucherEntities = new ArrayList<>();
-            CSVParser csvParser = CSVParser.parse(is.toString(), CSVFormat.RFC4180);
+            CSVParser csvParser = CSVParser.parse(is, StandardCharsets.UTF_8, CSVFormat.RFC4180);
 
 
             Iterable<CSVRecord> csvRecords = csvParser.getRecords();
