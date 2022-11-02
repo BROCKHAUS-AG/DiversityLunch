03ad8a6e473189ae06e190f49d6e7ab67ec54f04
tgohlisch <timgohlisch@gmail.com>
2022-11-02 17:42:48 +0100

    doc(MeetingProposalValidation): add a string documenting the functionality of  MeetingProposalValidation

--- diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/meeting/utils/MeetingProposalValidation.java
+++ diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/meeting/utils/MeetingProposalValidation.java
@@ -10,9 +10,16 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.TimeZone;
 
+/**
+ * Validates, if the given LocalDateTime is part of a certain array of times (12:30, 13:00, 13:30) which are considered
+ * to be lunchtime. Since the proposal are send in a ISO date string with GMT+0 a differentiation is needed to determine
+ * whether the given ISO date is in the lunchtime for the german GMT+1 time zone. The daylight saving time
+ * ("Sommer" meaning summer time, making Germany effectively part of the GMT+2 zone) has to be considered as well,
+ * leading to two different valid GMT+0 time arrays.
+ */
 public class MeetingProposalValidation implements ConstraintValidator<ProposedDateTimeValidator,LocalDateTime> {
-    String [] validSummerTimes = {"10:30","11:00","11:30"};
-    String [] validWinterTimes = {"11:30","12:00","12:30"};
+    private static final String [] validSummerTimes = {"10:30","11:00","11:30"};
+    private static final String [] validWinterTimes = {"11:30","12:00","12:30"};
 
     @Override
     public void initialize(ProposedDateTimeValidator proposedDateTimeValidator) {
