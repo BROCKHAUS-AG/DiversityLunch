ce32a529365915d5b776de24d1ff5b1019be10bf
tgohlisch <timgohlisch@gmail.com>
2022-11-04 09:49:16 +0100

    fix(DiversityLunchSecurityExpressionRoot): return Optional.empty() instead of null

--- diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/security/DiversityLunchSecurityExpressionRoot.java
+++ diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/security/DiversityLunchSecurityExpressionRoot.java
@@ -91,12 +91,12 @@ public class DiversityLunchSecurityExpressionRoot extends SecurityExpressionRoot
     public Optional<String> getOID(){
         OAuth2AuthenticatedPrincipal oAuth2Authentication = getOAuth2AuthenticatedPrincipal();
         if(oAuth2Authentication == null){
-            return null;
+            return Optional.empty();
         }
         Object claimValue = oAuth2Authentication.getAttribute("oid");
         if(claimValue == null){
             log.debug("No Claim with oid");
-            return null;
+            return Optional.empty();
         }
         return Optional.of(claimValue.toString());
     }
