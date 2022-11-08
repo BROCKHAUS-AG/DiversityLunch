c10f7e28212f63196022b9e9df4a1b2a4fd3fe54
Lucas Karle <lcskrl@googlemail.com>
2022-11-08 09:24:38 +0100

    test(FE AdminPanelTest):
    WIP

--- diversity-lunch-fe/src/components/AdminPanel/__test__/AdminPanel.test.tsx
+++ diversity-lunch-fe/src/components/AdminPanel/__test__/AdminPanel.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { FC, useEffect } from 'react';
 import { Provider, useDispatch } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
@@ -8,6 +8,7 @@ import { Role } from '../../../model/Role';
 import * as fetcher from '../../../utils/fetch.utils';
 import { mockedFetchGetAccount } from '../../../__global_test_data__/fetch';
 import { APP_STORE } from '../../../store/Store';
+import {Dispatch} from "@reduxjs/toolkit";
 
 const AccountLoader: FC = () => {
     const dispatch = useDispatch();
@@ -46,5 +47,12 @@ describe('Admin Panel', () => {
         renderContainer(Role.AZURE_ADMIN);
         const errorMessage = await screen.queryByText('Du bist kein Admin');
         expect(errorMessage).toBeNull();
+    });
+    it('should display an message when clicking the Send Testmail button', async () => {
+        jest.spyOn(fetcher, 'authenticatedFetchPost').mockImplementation(async () => new Response(null, { status: 200, statusText: 'Erfolgreich' }));
+        const button = await screen.queryAllByText('Testmail verschicken');
+        fireEvent.click(button[0]);
+        const errorMessage = await screen.queryByText('Erfolgreich gesendet');
+        expect(errorMessage).toBeInTheDocument();
     });
 });
