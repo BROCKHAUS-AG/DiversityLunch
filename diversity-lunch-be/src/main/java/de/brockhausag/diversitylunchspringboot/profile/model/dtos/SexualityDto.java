592aae7bed48f977310a8e571c1ee5d759d2a36c
Lucas Karle <lcskrl@googlemail.com>
2022-11-09 11:49:38 +0100

    feat(Model/DTO): added Model and Dto

---
+++ diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/profile/model/dtos/SexualityDto.java
@@ -0,0 +1,23 @@
+package de.brockhausag.diversitylunchspringboot.profile.model.dtos;
+
+import de.brockhausag.diversitylunchspringboot.profile.utils.baseApi.BaseDto;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class SexualityDto implements BaseDto {
+    private Long id;
+    @Schema(description = "The sexuality of a Person", example = "Asexual")
+    @Size(min=2, max=100, message = "Sexuality (descriptor) must be between 2 and 100 chars long")
+    @NotNull
+    @NotBlank
+    private String descriptor;
+}
