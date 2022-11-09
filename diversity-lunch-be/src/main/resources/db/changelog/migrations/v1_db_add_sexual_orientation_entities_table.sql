2765fb5a404db12aa90445ac547255167ce7e64e
Lucas Karle <lcskrl@googlemail.com>
2022-11-09 11:23:54 +0100

    feat(Migrations): Changed naming

--- diversity-lunch-be/src/main/resources/db/changelog/migrations/v1_db_add_sexual_orientation_entities_table.sql
+++ diversity-lunch-be/src/main/resources/db/changelog/migrations/v1_db_add_sexual_orientation_entities_table.sql
@@ -1,19 +1,19 @@
 --liquibase formatted sql
 
--- changeset dfuerst:17
+-- changeset dfuerst:v1-1
 CREATE TABLE IF NOT EXISTS sexual_orientation_entity (
    id BIGSERIAL PRIMARY KEY,
    descriptor VARCHAR(255)
 );
--- changeset dfuerst:18
+-- changeset dfuerst:v1-2
 INSERT INTO sexual_orientation_entity(descriptor) VALUES
-      ('Heterosexuell'),
-      ('Homesexuell'),
-    ('Bisexuell'),
-       ('Asexuell'),
+        ('Heterosexuell'),
+        ('Homesexuell'),
+        ('Bisexuell'),
+        ('Asexuell'),
         ('Pansexuell'),
         ('weitere Orientierungen'),
         ('keine Angabe');
 
--- changeset dfuerst:19
+-- changeset dfuerst:v1-3
 ALTER TABLE profile_entity ADD COLUMN sexual_orientation_id BIGINT CONSTRAINT fk_sexual_orientation REFERENCES sexual_orientation_entity (id) ON UPDATE NO ACTION ON DELETE NO ACTION;
