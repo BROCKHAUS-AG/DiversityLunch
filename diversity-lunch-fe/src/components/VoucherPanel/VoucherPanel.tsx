71671fe025a9c5b87c653e42caf6756b215f7697
cmeyer <cmeyer@brockhaus-ag.de>
2022-11-09 12:35:16 +0100

    feature(Voucherpanel): WIP Vouchercode to Frontend

--- diversity-lunch-fe/src/components/VoucherPanel/VoucherPanel.tsx
+++ diversity-lunch-fe/src/components/VoucherPanel/VoucherPanel.tsx
@@ -1,52 +1,53 @@
 import React, { useState } from 'react';
 import '../../styles/component-styles/voucherpanel/voucherPanel.scss';
 import { useSelector } from 'react-redux';
-import { Dispatch } from '@reduxjs/toolkit';
 import { DiversityIconContainer } from '../General/HeaderTemplate/DiversityIconContainer';
 import { Button } from '../General/Button/Button';
 import { AppStoreState } from '../../store/Store';
 import { ProfileState } from '../../data/profile/profile-state.type';
 import { authenticatedFetchGet, authenticatedFetchPut } from '../../utils/fetch.utils';
-import { globalErrorSlice } from '../../data/error/global-error-slice';
-import { Account } from '../../types/Account';
-import { accountsAction } from '../../data/accounts/accounts-reducer';
 
 export const VoucherPanel = () => {
     const [revealed, setRevealed] = useState(false);
     const profileState: ProfileState = useSelector((store: AppStoreState) => store.profile);
+
     let abc = String(window.location.pathname);
     abc = abc.substring(14, 100);
-    function claimVoucher(profileId: number, meetingId: number) {
-        return async () => {
-            try {
-                const response = await authenticatedFetchPut(`/api/voucher/claim/${profileId}/${meetingId}`, '');
 
-                if (response.ok) {
-                    //
-                } else {
-                    //
-                }
-            } catch (error) {
-                //
+    const claimVoucher = (profileId: number, meetingId: number) => async () => {
+        try {
+            const response = await authenticatedFetchPut(`/api/voucher/claim/${profileId}/${meetingId}`, '');
+
+            if (response.ok) {
+                return true;
             }
-        };
-    }
+            return false;
+        } catch (error) {
+            //
+        }
+    };
 
-    function getVoucher(profileId: number) {
-        return async () => {
-            try {
-                const response = await authenticatedFetchGet(`/api/voucher/get/${profileId}`);
+    const getVoucher = async (profileId: number, meetingId: number) => {
+        try {
+            const response = await authenticatedFetchGet(`/api/voucher/get/${profileId}`);
 
-                if (response.ok) {
-                    //
-                } else {
-                    //
-                }
-            } catch (error) {
-                //
+            if (response.ok) {
+                return response.json().then((jsonRes) => jsonRes.filter((data: { meetingId: number; }) => data.meetingId === meetingId));
             }
-        };
-    }
+            console.log('Fehler!!!!!');
+        } catch (error) {
+            return false;
+        }
+    };
+
+    const claim = () => {
+        if (claimVoucher(1, 3)) {
+            getVoucher(1, 3).then((key) => {
+                console.log(key);
+            });
+            setRevealed(true);
+        }
+    };
 
     return (
         <div className="ShowVoucher">
@@ -61,7 +62,7 @@ export const VoucherPanel = () => {
                             </span>
                         </div>
                     )
-                    : <Button label="FREISCHALTEN" onClick={() => setRevealed(true)} />
+                    : <Button label="FREISCHALTEN" onClick={() => claim()} />
             }
         </div>
     );
