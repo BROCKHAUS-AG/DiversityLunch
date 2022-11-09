90f8534f912b36ffc08311e87b2d1d2123e66df2
dfuerst <dfuerst@brockhaus-ag.de>
2022-11-09 07:49:10 +0100

    fix(Backend, VoucherController): changed http verb and added PreAuth

--- diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/voucher/controller/VoucherController.java
+++ diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/voucher/controller/VoucherController.java
@@ -27,11 +27,8 @@ public class VoucherController {
     private final VoucherService voucherService;
     private final VoucherMapper voucherMapper;
 
-
-    /*This should be a PUT however it will be called from within an email so this is only possible via GET*/
-
-    /*TODO make route publicly accessible*/
-    @GetMapping("/claim/{profileId}/{meetingId}")
+    @PreAuthorize("isProfileOwner(#profileId)")
+    @PutMapping("/claim/{profileId}/{meetingId}")
     public ResponseEntity<?> claimVoucher(@PathVariable Long profileId, @PathVariable Long meetingId) {
         try {
             Optional<VoucherEntity> voucherEntity = voucherService.getUnclaimedVoucherForMeeting(profileId, meetingId);
