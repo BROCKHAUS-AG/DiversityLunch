b044a841829cde44665ba313a97d31a20a1619bc
cmeyer <cmeyer@brockhaus-ag.de>
2022-11-10 11:25:14 +0100

    feature(Voucherpanel): WIP voucherClaim fix

--- diversity-lunch-fe/src/components/VoucherPanel/VoucherPanel.tsx
+++ diversity-lunch-fe/src/components/VoucherPanel/VoucherPanel.tsx
@@ -7,10 +7,12 @@ import { AppStoreState } from '../../store/Store';
 import { authenticatedFetchGet, authenticatedFetchPut } from '../../utils/fetch.utils';
 import { AccountState, AccountStateOk } from '../../data/account/account-state.type';
 import { Account } from '../../types/Account';
+import { PopUp } from '../AdminPanel/userAdministration/PopUp';
 
 export const VoucherPanel = () => {
     const [revealed, setRevealed] = useState(false);
     const [voucherCode, setVoucherCode] = useState('empty');
+    const [isError, setError] = useState(false);
     const accountState: AccountState = useSelector((store: AppStoreState) => store.account);
     const account: Account = (accountState as AccountStateOk).accountData;
 
@@ -19,10 +21,10 @@ export const VoucherPanel = () => {
         return Number(url.split('/').pop());
     };
 
-    const claimVoucher = (profileId: number, meetingId: number) => async () => {
+    const claimVoucher = async (profileId: number, meetingId: number) => {
         try {
             const response = await authenticatedFetchPut(`/api/voucher/claim/${profileId}/${meetingId}`, '');
-            return response.ok === true;
+            return response.ok;
         } catch (error) {
             // error
         }
@@ -30,45 +32,57 @@ export const VoucherPanel = () => {
     };
 
     const getVoucher = async (profileId: number, meetingId: number) => {
-        try {
-            const response = await authenticatedFetchGet(`/api/voucher/get/${profileId}`);
-            if (response.ok) {
-                return response.json().then((jsonRes) => jsonRes.filter((data: { meetingId: number; }) => data.meetingId === meetingId));
-            }
-        } catch (error) {
-            // error
-        }
-        return false;
+        const response = await authenticatedFetchGet(`/api/voucher/get/${profileId}`);
+        if (response.ok) {
+            return response.json().then((jsonRes) => jsonRes.filter((data: { meetingId: number; }) => data.meetingId === meetingId));
+        } throw Error();
     };
 
-    const retrieveVoucherCode = () => {
+    const retrieveVoucherCode = async () => {
         const meetingId = extractMeetingIdFromURL();
         const { profileId } = account;
-        if (meetingId > -1 && claimVoucher(profileId, meetingId)) {
-            getVoucher(profileId, meetingId).then((voucher) => {
+        if (meetingId > -1) {
+            await claimVoucher(profileId, meetingId);
+            try {
+                const voucher = await getVoucher(profileId, meetingId);
                 setVoucherCode(voucher[0].voucherCode);
                 setRevealed(true);
-            });
-            return;
-        }
-        // TBD: Hier sollte eine vernünftige Fehlermeldung hin
-        console.log('Fehlermeldung');
+            } catch (error) {
+                setError(true);
+            }
+        } else setError(true);
     };
 
     return (
         <div className="ShowVoucher">
             <DiversityIconContainer title="GUTSCHEIN" />
-            <p className="ShowVoucher-text">Hier kannst du deinen Gutschein bekommen</p>
             {
                 revealed
                     ? (
-                        <div className="ShowVoucher-code">
-                            <span className="ShowVoucher-code-text">
-                                { voucherCode }
-                            </span>
-                        </div>
+                        <>
+                            <p className="ShowVoucher-text">Dein persönlicher Gutschein-Code</p>
+                            <div className="ShowVoucher-code">
+                                <span className="ShowVoucher-code-text">
+                                    {voucherCode}
+                                </span>
+                            </div>
+                        </>
                     )
-                    : <Button label="FREISCHALTEN" onClick={() => retrieveVoucherCode()} />
+                    : (
+                        <>
+                            <p className="ShowVoucher-text">Hier kannst du deinen Gutschein bekommen</p>
+                            <Button label="FREISCHALTEN" onClick={() => retrieveVoucherCode()} />
+                        </>
+                    )
+            }
+            {
+                isError && (
+                    <PopUp
+                        onButtonClick={() => setError(false)}
+                        message="Bei der Abfrage ist ein Fehler aufgetreten"
+                        buttonText="Okay"
+                    />
+                )
             }
         </div>
     );
