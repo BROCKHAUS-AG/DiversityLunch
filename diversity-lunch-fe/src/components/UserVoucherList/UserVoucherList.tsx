2b1b934fba3831d838313b712e835357916b32bb
cmeyer <cmeyer@brockhaus-ag.de>
2022-11-15 15:39:25 +0100

    feature(UserVoucherList): finished UsersVoucherCode Site

--- diversity-lunch-fe/src/components/UserVoucherList/UserVoucherList.tsx
+++ diversity-lunch-fe/src/components/UserVoucherList/UserVoucherList.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import '../../styles/component-styles/UserVoucherList/UserVoucherList.scss';
 import { AccountState, AccountStateOk } from '../../data/account/account-state.type';
 import { AppStoreState } from '../../store/Store';
 import { Account } from '../../types/Account';
 import { authenticatedFetchGet } from '../../utils/fetch.utils';
 import { DiversityIconContainer } from '../General/HeaderTemplate/DiversityIconContainer';
+import { CloseSiteContainer } from '../General/HeaderTemplate/CloseSiteContainer';
 
 export const UserVoucherList = () => {
     const accountState: AccountState = useSelector((store: AppStoreState) => store.account);
@@ -22,10 +24,11 @@ export const UserVoucherList = () => {
 
     return (
         <div className="ShowVouchers">
+            <CloseSiteContainer />
             <DiversityIconContainer title="GUTSCHEIN-ÜBERSICHT" />
             <p className="ShowVoucher-text">Hier findest du deine persönlichen Gutscheine</p>
             <ul className="ShowVoucherList">
-                {voucherList.map((voucher: any) => (<li key={voucher.uuid}>{voucher.voucherCode}</li>))}
+                {voucherList.map((voucher: any) => (<li className="voucherItem" key={voucher.uuid}>{voucher.voucherCode}</li>))}
             </ul>
         </div>
     );
