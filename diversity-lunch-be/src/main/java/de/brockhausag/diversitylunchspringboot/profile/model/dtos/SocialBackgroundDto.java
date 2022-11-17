39efd493f9bdec65acfc29b0d72da202a6eb7fe5
Amin <adianati@brockhaus-ag.de>
2022-11-17 15:14:01 +0100

    test(SocialBackground): Added SQL schema for integration testing

--- diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/profile/model/dtos/SocialBackgroundDto.java
+++ diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/profile/model/dtos/SocialBackgroundDto.java
@@ -17,7 +17,7 @@ public class SocialBackgroundDto implements BaseDto {
     private Long id;
 
     @Schema(description = "Your socialBackground in years", example = "2 Jahre")
-    @Size(min=1, max=255, message = "socialBackground must be between 5 and 255 chars long")
+    @Size(min=1, max=255, message = "socialBackground must be between 1 and 255 chars long")
     @NotNull
     @NotBlank
     private String descriptor;
