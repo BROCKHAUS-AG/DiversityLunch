a6d5e012527b130660976d39f68b02d58ef85a18
tgohlisch <timgohlisch@gmail.com>
2022-11-17 11:06:45 +0100

    feat(SocialBackground): Add dto & entity

---
+++ diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/profile/model/dtos/SocialBackgroundDto.java
@@ -0,0 +1,24 @@
+package de.brockhausag.diversitylunchspringboot.profile.model.dtos;
+
+import de.brockhausag.diversitylunchspringboot.profile.utils.baseApi.BaseDto;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class SocialBackgroundDto implements BaseDto {
+    private Long id;
+
+    @Schema(description = "Your socialBackground in years", example = "2 Jahre")
+    @Size(min=1, max=255, message = "socialBackground must be between 5 and 255 chars long")
+    @NotNull
+    @NotBlank
+    private String descriptor;
+}
