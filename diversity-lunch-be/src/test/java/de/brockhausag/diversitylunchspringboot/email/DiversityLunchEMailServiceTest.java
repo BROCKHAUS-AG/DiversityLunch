26a72779e6ea480121861459a3e99264348677c5
dfuerst <dfuerst@brockhaus-ag.de>
2022-11-18 11:57:36 +0100

    feat(Backend, EmailServiceTest): Added TestCases for Email Generation

--- diversity-lunch-be/src/test/java/de/brockhausag/diversitylunchspringboot/email/DiversityLunchEMailServiceTest.java
+++ diversity-lunch-be/src/test/java/de/brockhausag/diversitylunchspringboot/email/DiversityLunchEMailServiceTest.java
@@ -1,17 +1,25 @@
 package de.brockhausag.diversitylunchspringboot.email;
 
+import de.brockhausag.diversitylunchspringboot.dataFactories.MeetingTestdataFactory;
+import de.brockhausag.diversitylunchspringboot.dataFactories.ProfileTestdataFactory;
 import de.brockhausag.diversitylunchspringboot.email.service.DiversityLunchEMailService;
+import de.brockhausag.diversitylunchspringboot.meeting.model.MeetingEntity;
+import de.brockhausag.diversitylunchspringboot.profile.model.entities.ProfileEntity;
 import de.brockhausag.diversitylunchspringboot.properties.DiversityLunchMailProperties;
+import de.brockhausag.diversitylunchspringboot.voucher.service.VoucherService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -29,6 +37,25 @@ class DiversityLunchEMailServiceTest {
     @InjectMocks
     DiversityLunchEMailService diversityLunchEMailService;
 
+    @Mock
+    VoucherService voucherService;
+
+   private final  ProfileTestdataFactory profileTestdataFactory = new ProfileTestdataFactory();
+    private final MeetingTestdataFactory meetingTestdataFactory = new MeetingTestdataFactory();
+
+    private ProfileEntity[] attendees;
+    private MeetingEntity meeting;
+
+    @Value("${diversity.url.baseUrl}")
+    private String BASE_URL;
+
+    @BeforeEach
+    void Setup(){
+        ProfileEntity proposer = profileTestdataFactory.buildEntity(1);
+        ProfileEntity partner = profileTestdataFactory.buildEntity(2);
+        attendees = new ProfileEntity[]{proposer, partner};
+        meeting = meetingTestdataFactory.matchedMeeting(proposer, partner);
+    }
 
     @Test
     void testSendEmail() throws MessagingException {
@@ -41,4 +68,47 @@ class DiversityLunchEMailServiceTest {
         verify(mailSender, times(1)).send(msg);
     }
 
+    @Test
+    void testCreateEMailTemplateHTML_expectsEmailWithClaimLink(){
+    when(voucherService.getAmountOfStoredVouchers()).thenReturn(1);
+
+    String claimLinkAnchorTag = String.format("<a href=%s>Hier klicken</a>", BASE_URL + "/voucherClaim/" + meeting.getId());
+
+    String email = diversityLunchEMailService.createEmailTemplateHTML(attendees, meeting);
+
+        assertTrue(email.contains(claimLinkAnchorTag));
+    }
+
+    @Test
+    void testCreateEMailTemplateHTML_expectsEmailWithoutClaimLink(){
+        when(voucherService.getAmountOfStoredVouchers()).thenReturn(0);
+
+        String claimLinkAnchorTag = String.format("<a href=%s>Hier klicken</a>", BASE_URL + "/voucherClaim/" + meeting.getId());
+
+        String email = diversityLunchEMailService.createEmailTemplateHTML(attendees, meeting);
+
+        assertFalse( email.contains(claimLinkAnchorTag));
+    }
+
+    @Test
+    void testCreateEMailTemplatePlain_expectsEmailWithClaimLink(){
+        when(voucherService.getAmountOfStoredVouchers()).thenReturn(1);
+
+        String claimLinkAnchorTag = String.format("Hier kannst du deinen Lieferando Gutschein anfordern: %s", BASE_URL + "/voucherClaim/" + meeting.getId());
+
+        String email = diversityLunchEMailService.createEmailTemplatePlain(attendees, meeting);
+
+        assertTrue(email.contains(claimLinkAnchorTag));
+    }
+
+    @Test
+    void testCreateEMailTemplatePlain_expectsEmailWithoutClaimLink(){
+        when(voucherService.getAmountOfStoredVouchers()).thenReturn(0);
+
+        String claimLinkAnchorTag = String.format("Hier kannst du deinen Lieferando Gutschein anfordern: %s", BASE_URL + "/voucherClaim/" + meeting.getId());
+
+        String email = diversityLunchEMailService.createEmailTemplatePlain(attendees, meeting);
+
+        assertFalse( email.contains(claimLinkAnchorTag));
+    }
 }
