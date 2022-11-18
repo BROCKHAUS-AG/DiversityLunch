e5952980785afe7b3f72533a0414b0c51eff3896
Amin <adianati@brockhaus-ag.de>
2022-11-18 09:55:19 +0100

    bug(SocialBackround): Table id for default value

--- diversity-lunch-be/src/main/resources/db/changelog/migrations/v3_db_add_social_background_entities_table.sql
+++ diversity-lunch-be/src/main/resources/db/changelog/migrations/v3_db_add_social_background_entities_table.sql
@@ -13,5 +13,7 @@ INSERT INTO social_background_entity(descriptor) VALUES
         ('keine Angabe');
 
 -- changeset adianati:v3-3
-ALTER TABLE profile_entity ADD COLUMN IF NOT EXISTS social_background_id BIGINT NOT NULL DEFAULT 7 CONSTRAINT fk_social_background
+ALTER TABLE profile_entity ADD COLUMN IF NOT EXISTS social_background_id BIGINT
+    NOT NULL DEFAULT 3
+    CONSTRAINT fk_social_background
     REFERENCES social_background_entity (id) ON UPDATE NO ACTION ON DELETE NO ACTION;
