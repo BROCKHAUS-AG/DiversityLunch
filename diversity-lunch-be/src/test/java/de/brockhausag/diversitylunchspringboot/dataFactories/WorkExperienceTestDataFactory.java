c35314a9d2ebf87956ace3447453bb365a842aae
Darren Fürst <77537563+N0tAScooby@users.noreply.github.com>
2022-11-21 09:50:27 +0100

    removed TODO

--- diversity-lunch-be/src/test/java/de/brockhausag/diversitylunchspringboot/dataFactories/WorkExperienceTestDataFactory.java
+++ diversity-lunch-be/src/test/java/de/brockhausag/diversitylunchspringboot/dataFactories/WorkExperienceTestDataFactory.java
@@ -7,7 +7,6 @@ public class WorkExperienceTestDataFactory {
     private static final  int numberOfCompleteSets = 3;
     private static final Long[] ids = {666L, 1L, 2L, 3L};
 
-    //TODO: global test data dfuerst-18.11.22         User Story : 17923
 
     private static final String[] descriptors =  {"incomplete","0-3 Jahre", "4-10 Jahre", "über 10 Jahre"};
 
