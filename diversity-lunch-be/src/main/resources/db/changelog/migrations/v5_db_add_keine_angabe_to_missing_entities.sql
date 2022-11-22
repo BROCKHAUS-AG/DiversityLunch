185ab5685ed091ee7e3f04bdcd950e18ae4cba75
Darren Fürst <77537563+N0tAScooby@users.noreply.github.com>
2022-11-22 12:54:36 +0100

    Feat/17843 keine angabe in allen feldern (#53)
    * fix(Backend, Dimensions): added 'keine Angabe' to all except birth year
    
    * fix(Dimensions): sorting options added
    
    * style(profile-form): fix lint errors
    
    Co-authored-by: dfuerst <dfuerst@brockhaus-ag.de>
    Co-authored-by: Tim <53128790+Gohlisch@users.noreply.github.com>
    Co-authored-by: tgohlisch <timgohlisch@gmail.com>

---
+++ diversity-lunch-be/src/main/resources/db/changelog/migrations/v5_db_add_keine_angabe_to_missing_entities.sql
@@ -0,0 +1,16 @@
+--liquibase formatted sql
+
+-- changeset dfuerst:v5-1
+INSERT INTO country_entity(descriptor) VALUES ('keine Angabe');
+INSERT INTO diet_entity(descriptor) VALUES ('keine Angabe');
+INSERT INTO education_entity(descriptor) VALUES ('keine Angabe');
+INSERT INTO hobby_category_entity(descriptor) VALUES ('keine Angabe');
+INSERT INTO hobby_entity(descriptor, category_id) VALUES ('keine Angabe', (SELECT id FROM hobby_category_entity WHERE descriptor LIKE 'keine Angabe'));
+INSERT INTO language_entity(descriptor) VALUES ('keine Angabe');
+INSERT INTO project_entity(descriptor) VALUES ('keine Angabe');
+INSERT INTO religion_entity(descriptor) VALUES ('keine Angabe');
+INSERT INTO work_experience_entity(descriptor) VALUES ('keine Angabe');
+
+
+--changeset dfuerst:v5-2
+Update gender_entity SET descriptor='keine Angabe' WHERE descriptor='Keine Angabe';
