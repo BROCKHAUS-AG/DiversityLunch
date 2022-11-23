3d4c40c33289558e4ba6f935a6a59a385deb3c6d
FelixHausdorff <95748406+FelixHausdorff@users.noreply.github.com>
2022-11-23 15:59:39 +0100

    added link to cron job name sheme documentation

--- diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/config/SchedulingConfig.java
+++ diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/config/SchedulingConfig.java
@@ -20,6 +20,8 @@ public class SchedulingConfig {
 
     private final MatchingService matchingService;
     private final MeetingProposalRepository meetingProposalRepository;
+    
+    //Note: Cronjob naming sheme https://spring.io/blog/2020/11/10/new-in-spring-5-3-improved-cron-expressions
 
 
     @Scheduled(cron = "0 0 2 * * *")
