adbf5d3910f068ae94854636a12d445c73c41242
Lucas Karle <lcskrl@googlemail.com>
2022-11-25 15:09:49 +0100

    refactor(Matching Utils): WIP

--- diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/profile/logic/HobbyCategoryService.java
+++ diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/profile/logic/HobbyCategoryService.java
@@ -3,11 +3,16 @@ package de.brockhausag.diversitylunchspringboot.profile.logic;
 import de.brockhausag.diversitylunchspringboot.profile.data.HobbyCategoryRepository;
 import de.brockhausag.diversitylunchspringboot.profile.model.entities.HobbyCategoryEntity;
 import de.brockhausag.diversitylunchspringboot.generics.BasicDimension.BaseEntityService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class HobbyCategoryService extends BaseEntityService<HobbyCategoryEntity, HobbyCategoryRepository> {
-    public HobbyCategoryService(HobbyCategoryRepository repository) {
-        super(repository);
+@RequiredArgsConstructor
+public class HobbyCategoryService {
+    HobbyCategoryRepository repository;
+    public Optional<HobbyCategoryEntity> getEntityById(long id){
+        return repository.findById(id);
     }
 }
