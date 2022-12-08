32c0d966ef3fb5a66c2cb04f46420743be443f8a
jklein <justin.klein@brockhaus-ag.de>
2022-12-08 13:55:40 +0100

    refactor(BE Scheduling): Cleaned up the cron definition
    Signed-off-by: jklein <justin.klein@brockhaus-ag.de>

--- diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/config/SchedulingConfig.java
+++ diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/config/SchedulingConfig.java
@@ -4,10 +4,9 @@ import de.brockhausag.diversitylunchspringboot.match.service.MatchingService;
 import de.brockhausag.diversitylunchspringboot.meeting.model.MeetingProposalEntity;
 import de.brockhausag.diversitylunchspringboot.meeting.repository.MeetingProposalRepository;
 import de.brockhausag.diversitylunchspringboot.meeting.service.MeetingService;
-import de.brockhausag.diversitylunchspringboot.meeting.service.MsTeamsService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
@@ -19,6 +18,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @Configuration
 @EnableScheduling
+@Slf4j
 public class SchedulingConfig {
 
     private final MatchingService matchingService;
@@ -26,7 +26,7 @@ public class SchedulingConfig {
     private final MeetingService meetingService;
 
     // Note: Cronjob naming scheme https://spring.io/blog/2020/11/10/new-in-spring-5-3-improved-cron-expressions
-    @Scheduled(cron = "#{'${diversity.settings.matchingCronJob}' > '' ? '${diversity.settings.matchingCronJob}' : '0 0 2 * * *'}") // Every Day at 02:00
+    @Scheduled(cron = "${diversity.settings.scheduling.matchingCronJob}") // Every Day at 02:00
     public void scheduleMatching() {
         LocalDate date = LocalDate.now();
         LocalDateTime dateTime = date.atTime(0, 0);
@@ -36,12 +36,14 @@ public class SchedulingConfig {
                 .map(MeetingProposalEntity::getProposedDateTime)
                 .distinct()
                 .toList();
+        log.info("Running scheduled Matching at " + dateTime);
         allMatchTimes.forEach(time -> matchingService.matching(time, dateTime));
     }
 
-    @Scheduled(cron = "#{'${diversity.settings.meetingReminderCronJob}' > '' ? '${diversity.settings.meetingReminderCronJob}' : '0 0/30 * * * *'}") // Every Full and Half Hour
+    @Scheduled(cron = "${diversity.settings.scheduling.meetingReminderCronJob}") // Every Day at 02:00
     public void scheduleMailBeforeMeetingSending() {
         LocalDateTime dateTime = LocalDateTime.now(ZoneOffset.UTC);
+        log.info("Running scheduled sending of Meeting-Reminders at " + dateTime);
         matchingService.sendQuestions(dateTime);
     }
 
