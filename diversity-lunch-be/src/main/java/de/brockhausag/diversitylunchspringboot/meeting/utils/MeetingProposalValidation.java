695c597aec1660a329f640e4e87f44990e05417d
Fabio <96080826+En3rMaXx2@users.noreply.github.com>
2022-12-15 15:28:02 +0100

    Zusätzlicher zeitslot (#96)
    * feat(BE, MeetingProposal): Added an additional time slot (12:30)
    
    * feat(FE select-meeting-data): added Button
    
    * fix(BE MeetingProposalValidation): fixed validTimeslots
    
    * fix(BE MeetingProposalValidationTest): fixed tests
    
    Co-authored-by: Amin <adianati@brockhaus-ag.de>

--- diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/meeting/utils/MeetingProposalValidation.java
+++ diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/meeting/utils/MeetingProposalValidation.java
@@ -7,20 +7,25 @@ import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
-import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.TimeZone;
 
-/**
- * Validates, if the given LocalDateTime is part of a certain array of times (12:30, 13:00, 13:30) which are considered
- * to be lunchtime. Since the proposal are send in a ISO date string with GMT+0 a differentiation is needed to determine
- * whether the given ISO date is in the lunchtime for the german GMT+1 time zone. The daylight saving time
- * ("Sommerzeit", meaning summer time) making Germany effectively part of the GMT+2 zone) has to be considered as well,
- * leading to two different valid GMT+0 time arrays.
- */
 public class MeetingProposalValidation implements ConstraintValidator<ProposedDateTimeValidator, LocalDateTime> {
-    private static final LocalTime[] validSummerTimes = {LocalTime.of(10, 30), LocalTime.of(11, 0), LocalTime.of(11, 30)};
-    private static final LocalTime[] validWinterTimes = {LocalTime.of(11, 30), LocalTime.of(12, 0), LocalTime.of(12, 30)};
+
+    /*
+     * Validates, if the given LocalDateTime is part of a certain array of times (12:30, 13:00, 13:30) which are considered
+     * to be lunchtime. Since the proposal are send in a ISO date string with GMT+0 a differentiation is needed to determine
+     * whether the given ISO date is in the lunchtime for the german GMT+1 time zone. The daylight saving time
+     * ("Sommerzeit", meaning summer time) making Germany effectively part of the GMT+2 zone) has to be considered as well,
+     * leading to two different valid GMT+0 time arrays.
+     */
+    private static final List<LocalTime> validTimeslots = List.of(
+            LocalTime.of(10, 0),
+            LocalTime.of(10, 30),
+            LocalTime.of(11, 0),
+            LocalTime.of(11, 30)
+            );
 
     private static boolean isSummerTime(LocalDateTime proposedDateTime) {
         final TimeZone europeBerlinTimeZone = TimeZone.getTimeZone(ZoneId.of("Europe/Berlin"));
@@ -34,7 +39,9 @@ public class MeetingProposalValidation implements ConstraintValidator<ProposedDa
 
     @Override
     public boolean isValid(LocalDateTime proposedDateTime, ConstraintValidatorContext cxt) {
-        final LocalTime[] validTimes = isSummerTime(proposedDateTime) ? validSummerTimes : validWinterTimes;
-        return Arrays.stream(validTimes).anyMatch(t -> t.equals(proposedDateTime.toLocalTime()));
+        final List<LocalTime> validTimes = isSummerTime(proposedDateTime) ?
+                validTimeslots :
+                validTimeslots.stream().map(timeSlot -> timeSlot.plusHours(1)).toList();
+        return validTimes.stream().anyMatch(t -> t.equals(proposedDateTime.toLocalTime()));
     }
 }
