2dabf5d9a8491859168bac14b8e5288c584bbeab
Fabio <fabioleidheuser@gmail.com>
2022-12-15 15:01:56 +0100

    fix(BE MeetingProposalValidation): fixed validTimeslots

--- diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/meeting/utils/MeetingProposalValidation.java
+++ diversity-lunch-be/src/main/java/de/brockhausag/diversitylunchspringboot/meeting/utils/MeetingProposalValidation.java
@@ -1,31 +1,30 @@
 package de.brockhausag.diversitylunchspringboot.meeting.utils;
 
 import de.brockhausag.diversitylunchspringboot.meeting.customValidators.ProposedDateTimeValidator;
-import org.apache.tomcat.jni.Local;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.TimeZone;
 
-/**
- * Validates, if the given LocalDateTime is part of a certain array of times (12:30, 13:00, 13:30) which are considered
- * to be lunchtime. Since the proposal are send in a ISO date string with GMT+0 a differentiation is needed to determine
- * whether the given ISO date is in the lunchtime for the german GMT+1 time zone. The daylight saving time
- * ("Sommerzeit", meaning summer time) making Germany effectively part of the GMT+2 zone) has to be considered as well,
- * leading to two different valid GMT+0 time arrays.
- */
 public class MeetingProposalValidation implements ConstraintValidator<ProposedDateTimeValidator, LocalDateTime> {
+
+    /*
+     * Validates, if the given LocalDateTime is part of a certain array of times (12:30, 13:00, 13:30) which are considered
+     * to be lunchtime. Since the proposal are send in a ISO date string with GMT+0 a differentiation is needed to determine
+     * whether the given ISO date is in the lunchtime for the german GMT+1 time zone. The daylight saving time
+     * ("Sommerzeit", meaning summer time) making Germany effectively part of the GMT+2 zone) has to be considered as well,
+     * leading to two different valid GMT+0 time arrays.
+     */
     private static final List<LocalTime> validTimeslots = List.of(
+            LocalTime.of(10, 0),
             LocalTime.of(10, 30),
             LocalTime.of(11, 0),
-            LocalTime.of(11, 30),
-            LocalTime.of(12, 0)
+            LocalTime.of(11, 30)
             );
 
     private static boolean isSummerTime(LocalDateTime proposedDateTime) {
