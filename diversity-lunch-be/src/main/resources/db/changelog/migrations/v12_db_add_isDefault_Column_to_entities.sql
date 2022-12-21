6d142876bfe3bbf2bb7a6ec7c75816aa8f71a460
Amin <adianati@brockhaus-ag.de>
2022-12-21 11:00:41 +0100

    fix(BE, Migrationskripts): Fixed wrong table names

--- diversity-lunch-be/src/main/resources/db/changelog/migrations/v12_db_add_isDefault_Column_to_entities.sql
+++ diversity-lunch-be/src/main/resources/db/changelog/migrations/v12_db_add_isDefault_Column_to_entities.sql
@@ -2,27 +2,27 @@
 
 
 -- changeset dfuerst:v12-1
-ALTER TABLE country_entity ADD COLUMN isDefault boolean not null default false;
-ALTER TABLE diet_entity ADD COLUMN isDefault boolean not null default false;
-ALTER TABLE education_entity ADD COLUMN isDefault boolean not null default false;
-ALTER TABLE gender_entity ADD COLUMN isDefault boolean not null default false;
-ALTER TABLE language_entity ADD COLUMN isDefault boolean not null default false;
-ALTER TABLE religion_entity ADD COLUMN isDefault boolean not null default false;
-ALTER TABLE sexual_orientation_entity ADD COLUMN isDefault boolean not null default false;
-ALTER TABLE social_background_discrimination_entity ADD COLUMN isDefault boolean not null default false;
-ALTER TABLE work_experience_entity ADD COLUMN isDefault boolean not null default false;
-ALTER TABLE project_entity ADD COLUMN isDefault boolean not null default false;
-ALTER TABLE social_background_entity ADD COLUMN isDefault boolean not null default false;
+ALTER TABLE country_entity ADD COLUMN is_default boolean not null default false;
+ALTER TABLE diet_entity ADD COLUMN is_default boolean not null default false;
+ALTER TABLE education_entity ADD COLUMN is_default boolean not null default false;
+ALTER TABLE gender_entity ADD COLUMN is_default boolean not null default false;
+ALTER TABLE language_entity ADD COLUMN is_default boolean not null default false;
+ALTER TABLE religion_entity ADD COLUMN is_default boolean not null default false;
+ALTER TABLE sexual_orientation_entity ADD COLUMN is_default boolean not null default false;
+ALTER TABLE social_background_discrimination_entity ADD COLUMN is_default boolean not null default false;
+ALTER TABLE work_experience_entity ADD COLUMN is_default boolean not null default false;
+ALTER TABLE project_entity ADD COLUMN is_default boolean not null default false;
+ALTER TABLE social_background_entity ADD COLUMN is_default boolean not null default false;
 
 -- changeset dfuerst:v12-2
-Update country_entity                          SET isDefault=true   WHERE lower(descriptor)=lower('keine angabe');
-Update diet_entity                             SET isDefault=true   WHERE lower(descriptor)=lower('keine angabe');
-Update education_entity                        SET isDefault=true   WHERE lower(descriptor)=lower('keine angabe');
-Update gender_entity                           SET isDefault=true   WHERE lower(descriptor)=lower('keine angabe');
-Update language_entity                         SET isDefault=true   WHERE lower(descriptor)=lower('keine angabe');
-Update religion_entity                         SET isDefault=true   WHERE lower(descriptor)=lower('keine angabe');
-Update sexual_orientation_entity               SET isDefault=true   WHERE lower(descriptor)=lower('keine angabe');
-Update social_background_discrimination_entity SET isDefault=true   WHERE lower(descriptor)=lower('keine angabe');
-Update work_experience_entity                  SET isDefault=true   WHERE lower(descriptor)=lower('keine angabe');
-Update social_background_entity                SET isDefault=true   WHERE lower(descriptor)=lower('keine angabe');
-Update project_entity                          SET isDefault=true   WHERE lower(descriptor)=lower('keine angabe');
+Update country_entity                          SET is_default=true   WHERE lower(descriptor)=lower('keine angabe');
+Update diet_entity                             SET is_default=true   WHERE lower(descriptor)=lower('keine angabe');
+Update education_entity                        SET is_default=true   WHERE lower(descriptor)=lower('keine angabe');
+Update gender_entity                           SET is_default=true   WHERE lower(descriptor)=lower('keine angabe');
+Update language_entity                         SET is_default=true   WHERE lower(descriptor)=lower('keine angabe');
+Update religion_entity                         SET is_default=true   WHERE lower(descriptor)=lower('keine angabe');
+Update sexual_orientation_entity               SET is_default=true   WHERE lower(descriptor)=lower('keine angabe');
+Update social_background_discrimination_entity SET is_default=true   WHERE lower(descriptor)=lower('keine angabe');
+Update work_experience_entity                  SET is_default=true   WHERE lower(descriptor)=lower('keine angabe');
+Update social_background_entity                SET is_default=true   WHERE lower(descriptor)=lower('keine angabe');
+Update project_entity                          SET is_default=true   WHERE lower(descriptor)=lower('keine angabe');
