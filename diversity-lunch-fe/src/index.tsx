9e1908d63f2f92463a51d9cbdb3c21faa41a85ec
dwolz <darian.wolz@brockhaus-ag.de>
2023-01-18 10:34:16 +0100

    refac / fe / dw remove "OfflineMode" to skip AuthenticationCheck

--- diversity-lunch-fe/src/index.tsx
+++ diversity-lunch-fe/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 import './resources/fonts/GilroyBlack.otf';
-import { App } from './user-interface/pages/app';
 
 // Styles
 import './global-styles/index.scss';
@@ -12,15 +11,10 @@ import './global-styles/index.scss';
 import { APP_STORE } from './data/app-store';
 import { AuthenticationCheck } from './user-interface/components/authentication-check/authentication-check';
 
-// SET TRUE FOR OFFLINE USE DEBUG
-const isOffline = false;
-
-export const OffMode = () => (isOffline ? <App /> : <AuthenticationCheck />);
-
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={APP_STORE}>
-            <OffMode />
+            <AuthenticationCheck />
         </Provider>
     </React.StrictMode>,
     document.getElementById('root'),
